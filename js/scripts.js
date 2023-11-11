18e71d2f9f554f603dd460fc3db22af6d0e41097
Padma Patil <padma@email.com>
2023-11-10 17:53:51 -0800

    Add generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function setToVariable() {
 
+    const click = document.getElementById("click");
     const h1 = document.createElement("h1");
     const p = document.createElement("p");
     const form = document.createElement("form");
@@ -16,27 +17,27 @@ function setToVariable() {
     p.append("Share your favorite things with us:");
 
     label1.setAttribute("for", "food");
-    label1.append("What is your favorite food?");
+    label1.append("What is your favorite food:");
     input1.setAttribute("id", "food");
     input1.setAttribute("type", "text");
     input1.setAttribute("name", "food");
 
     label2.setAttribute("for", "movie");
-    label2.append("What is your favorite movie?");
+    label2.append("What is your favorite movie:");
     input2.setAttribute("id", "movie");
     input2.setAttribute("type", "text");
     input2.setAttribute("name", "movie");
 
     label3.setAttribute("for", "book");
-    label3.append("What is your favorite book?");
+    label3.append("What is your favorite book:");
     input3.setAttribute("id", "book");
     input3.setAttribute("type", "text");
     input3.setAttribute("name", "book");
 
-    button.setAttribute("type", "submit");
+    button.setAttribute("type", "button");
     button.append("Submit");
 
-    form.append(label1, input1, label2, input2, label3, input3, button);
+    form.append(label1, input1, document.createElement("br"), label2, input2, document.createElement("br"), label3, input3, document.createElement("br"), button);
 
     document.body.append(h1);
     document.body.append(p);
@@ -46,7 +47,11 @@ function setToVariable() {
 
 
 
-window.addEventListener("load", function (event) {
-    event.preventDefault
-    setToVariable();
+window.addEventListener("load", function () {
+
+    click.addEventListener("click", function (event) {
+        event.preventDefault();
+        setToVariable();
+        document.getElementById("click").setAttribute("class", "hidden");
+    })
 });
